5a63107df655c6102415d3018f134cbe7b9f1b2e
KaelHe1212 <69683804+KaelHe1212@users.noreply.github.com>
2020-08-15 00:47:04 +0800

    TestDemo

---
+++ CiGATestDemo/Assets/PlayerInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+
+
+
+
+    public Vector3 GetMoveInput()
+    {
+
+        return new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0f);
+
+    }
+
+
+
+
+
+
+
+
+
+}
