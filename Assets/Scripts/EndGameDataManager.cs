ed460fb6b171cccb8a69d1e82a1f61111a4783b1
KaelHe1212 <69683804+KaelHe1212@users.noreply.github.com>
2020-08-16 03:53:17 +0800

    index on master: e980f6f Merge branch 'temp'

---
+++ Assets/Scripts/EndGameDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameDataManager : MonoBehaviour
+{
+    Text hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = GetComponent<Text>();
+        hp.text = ("你还剩"+GameManager.getGM.HP+"钱");
+    }
+
+    
+   
+}
